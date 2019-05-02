22babae258312c2fa43d1657983da76f3976cbdf
Yonel Meza Avila <yonelmeza@gmail.com>
2019-05-01 23:52:41 -0500

    Added display component

---
+++ src/components/display.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import styles from './keypad.module.scss';
+
+export default class Display extends Component {
+  renderMemoryIndicator() {
+    const { memory = false } = this.props;
+    if (memory !== false) {
+      return <span className={styles.memory}>M</span>;
+    }
+    return null;
+  }
+  render() {
+    const { value } = this.props;
+    return (
+      <div className={styles.display}>
+        {this.renderMemoryIndicator()}
+        <span className={styles.number}>{value}</span>
+      </div>
+    );
+  }
+}
---
+++ src/components/display.tests.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Display from './display';
+
+describe('Display component', () => {
+  test('Should show the value props', () => {
+    const value = (Math.random() * 1000000).toFixed(0);
+    const wrapper = shallow(<Display value={value} />);
+    expect(wrapper.find('span.number').text()).toEqual(value);
+  });
+  test('Should show memory indicator', () => {
+    const value = '00000000000';
+    const wrapper = shallow(<Display memory value={value} />);
+    expect(wrapper.find('span.memory').text()).toEqual('M');
+  });
+});
