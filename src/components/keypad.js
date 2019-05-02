17e7e497e9f4dd3d9829831454d935e6df0288c6
Yonel Meza Avila <yonelmeza@gmail.com>
2019-05-01 23:40:51 -0500

    Added keypad component

---
+++ src/components/keypad.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Key from './key';
+
+import styles from './keypad.module.scss';
+
+export default class Keypad extends Component {
+  constructor(props) {
+    super(props);
+    this.onNumberClick = this.onNumberClick.bind(this);
+    this.onOperatorClick = this.onOperatorClick.bind(this);
+  }
+  onNumberClick(number) {
+    this.props.selectNumber(number);
+  }
+  onOperatorClick(number) {
+    this.props.selectOperator(number);
+  }
+  render() {
+    const numberKeyProps = {
+      press: this.onNumberClick,
+      className: styles.key,
+    };
+    const operatorKeyProps = {
+      press: this.onOperatorClick,
+      className: styles.key,
+    };
+    return (
+      <div className={styles.keypad}>
+        <div className={styles.numberKeysContainer}>
+          <Key {...numberKeyProps} value={1} />
+          <Key {...numberKeyProps} value={2} />
+          <Key {...numberKeyProps} value={3} />
+          <Key {...numberKeyProps} value={4} />
+          <Key {...numberKeyProps} value={5} />
+          <Key {...numberKeyProps} value={6} />
+          <Key {...numberKeyProps} value={7} />
+          <Key {...numberKeyProps} value={8} />
+          <Key {...numberKeyProps} value={9} />
+          <Key {...numberKeyProps} value={0} />
+        </div>
+        <div className={styles.operatorKeysContainer}>
+          <Key {...operatorKeyProps} value={'+'} />
+          <Key {...operatorKeyProps} value={'-'} />
+          <Key {...operatorKeyProps} value={'*'} />
+          <Key {...operatorKeyProps} value={'/'} />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/keypad.tests.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Keypad from './keypad';
+import Key from './key';
+
+const operators = ['+', '-', '*', '/'];
+describe('Keypad component', () => {
+  test('Should render 0 to 9 keys', () => {
+    const wrapper = shallow(<Keypad />);
+    const keys = [];
+    for (let i = 0; i < 10; i++) {
+      keys.push(<Key value={i} />);
+    }
+    expect(wrapper.containsAllMatchingElements(keys)).toBeTruthy();
+  });
+  test('Should render + - * / keys', () => {
+    const wrapper = shallow(<Keypad />);
+    const keys = operators.map(key => {
+      return <Key value={key} />;
+    });
+    expect(wrapper.containsAllMatchingElements(keys)).toBeTruthy();
+  });
+  test('Should call selectNumber when you press a number key', () => {
+    const selectNumber = jest.fn();
+    for (let i = 0; i < 10; i++) {
+      selectNumber.mockReset();
+      const keyToPress = `${i}`;
+      const wrapper = mount(<Keypad selectNumber={selectNumber} />);
+      expect(selectNumber).toBeCalledTimes(0);
+      const key = wrapper.find(Key).findWhere(obj => {
+        return obj.text() === keyToPress;
+      });
+      key.first().simulate('click');
+      expect(selectNumber).toBeCalledTimes(1);
+      expect(selectNumber).toBeCalledWith(parseInt(keyToPress));
+    }
+  });
+
+  test('Should call selectOperator when you press a operator key', () => {
+    const selectOperator = jest.fn();
+    operators.forEach(operator => {
+      selectOperator.mockReset();
+      const keyToPress = operator;
+      const wrapper = mount(<Keypad selectOperator={selectOperator} />);
+      expect(selectOperator).toBeCalledTimes(0);
+      const key = wrapper.find(Key).findWhere(obj => {
+        return obj.text() === keyToPress;
+      });
+      key.first().simulate('click');
+      expect(selectOperator).toBeCalledTimes(1);
+      expect(selectOperator).toBeCalledWith(operator);
+    });
+  });
+});
